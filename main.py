b3732bb21306800fc4acc879db64a89769558ec7
BiteDev <113308968+BiteDev8@users.noreply.github.com>
2022-09-16 23:28:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ print(ASCII_art_1)
 
 
 def variant1(token):
-    response = get('https://discord.com/api/v6/auth/login', headers={"Authorization": token})#Bad variant for mass token check due to the rate limit.
+    response = get('https://discord.com/api/v10/auth/login', headers={"Authorization": token})#Bad variant for mass token check due to the rate limit.
     return True if response.status_code == 200 else False
 
 def variant2(token):
-    response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
+    response = post(f'https://discord.com/api/v10/invite/{randint(1,9999999)}', headers={'Authorization': token})
     if "You need to verify your account in order to perform this action." in str(response.content) or "401: Unauthorized" in str(response.content):
         return False
     else:
         return True
 
 def variant2_Status(token):
-    response = post(f'https://discord.com/api/v6/invite/{randint(1,9999999)}', headers={'Authorization': token})
+    response = post(f'https://discord.com/api/v10/invite/{randint(1,9999999)}', headers={'Authorization': token})
     if response.status_code == 401:
         return 'Invalid'
     elif "You need to verify your account in order to perform this action." in str(response.content):
